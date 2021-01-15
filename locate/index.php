851044f33703d56c205206f31798d5d866e18441
Edwin Masripan <edwin@laman7.com>
2021-01-15 11:10:39 +0800

    Update index.php

--- locate/index.php
+++ locate/index.php
@@ -44,10 +44,7 @@
     <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js" defer></script>
     <script src="leaflet-provider.js" defer></script>
     <script>
-        var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-});
+        L.tileLayer.provider('Stadia').addTo(map);
     </script>
     <script src="app.js" defer></script>
 
